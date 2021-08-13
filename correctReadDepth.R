d661b5090a4e804cb44b88e494b6d36b3571553e
Jiwoong Kim <jiwoong.kim@utsouthwestern.edu>
2021-08-13 05:32:07 -0500

    first commit

--- correctReadDepth.R
+++ correctReadDepth.R
@@ -1,9 +1,17 @@
 args <- commandArgs(TRUE)
 
 library(TitanCNA)
+library(GenomeInfoDb)
+library(GenomicRanges)
+library(HMMcopy)
+
+source("/archive/PCDC/shared/jkim23/src/TitanCNA/R/utils.R")
+#    names(targetIR) <- setGenomeStyle(names(targetIR), genomeStyle)
+#    seqnames(targetIR) <- setGenomeStyle(seqnames(targetIR), genomeStyle)
+
 if(length(args) == 6) {
-	exons <- read.delim(args[6], header = FALSE, as.is = TRUE)
-	write.table(correctReadDepth(args[1], args[2], args[3], args[4], genomeStyle = "UCSC", targetedSequence = exons), file = args[5], quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+	targetedSequence <- read.delim(args[6], header = FALSE, as.is = TRUE)
+	write.table(correctReadDepth(args[1], args[2], args[3], args[4], genomeStyle = "UCSC", targetedSequence = targetedSequence), file = args[5], quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 } else {
 	write.table(correctReadDepth(args[1], args[2], args[3], args[4], genomeStyle = "UCSC"), file = args[5], quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 }
